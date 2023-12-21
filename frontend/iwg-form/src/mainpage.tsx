00bb0b3b5f8ed370691a62e6dc23e62ea5c64528
khanabdu25 <khanabdu@umich.edu>
2023-12-21 04:11:49 -0500

    Added Selection of forms & favicons
    - added row that contains form options
    - added favicons and changed browser tab text

--- frontend/iwg-form/src/mainpage.tsx
+++ frontend/iwg-form/src/mainpage.tsx
@@ -1,15 +1,58 @@
 import React from 'react';
-import './mainpage.css'; // Create a separate CSS file for MainPage styles if needed
+import './mainpage.css';
+import './formcomponents.css';
+import imageSrc from './assets/tmc-logo.jpeg';
+import CheckWithText from './components/checkwithtext';
+import Spacer from './components/spacer';
 
 const MainPage: React.FC = () => {
-  return (
-    <main id="mainpage">
-    <div className="form-container">
-      <h1 className="form-heading">Report an Incident</h1>
-      {/* The rest of your form will go here */}
-    </div>
-  </main>
-  );
+    return (
+        <main id="mainpage">
+            <div className="form-container">
+                {/* header-logo row div */}
+                <div className="parent">
+                    {/* header div */}
+                    <div className="child">
+                        <h1 className="form-heading">Report an Incident</h1>
+                    </div>
+                    {/* image div */}
+                    <div className="child">
+                        <img
+                            style={{
+                                maxWidth: '60px',
+                                maxHeight: '61px',
+                                height: 'auto',
+                                width: 'auto',
+                                flexShrink: 0
+                            }}
+                            src={imageSrc}
+                            alt="Description"
+                        />
+                    </div>
+                </div>
+                
+                <div>
+                    <span className="asterisk">*</span>
+                    <span className="information"> </span>
+                    <span className="required">Required</span>
+                </div>
+                <Spacer height={20} />
+                <div className="subheader">Send my report to:</div>
+                <Spacer height={10} />
+                <div className='parent'>
+                    <div className="child">
+                        <CheckWithText text="CAIR" />
+                    </div>
+                    <div className="child">
+                        <CheckWithText text="DPSS" />
+                    </div>
+                    <div className="child">
+                        <CheckWithText text="Third" />
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default MainPage;
