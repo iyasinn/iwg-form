3ae170b93515463033a501761a0d3a2bb6007a59
iyasin <iayasin771@gmail.com>
2023-12-26 17:36:41 -0500

    finished

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import MainPage from './mainpage.tsx';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <MainPage />
+    </div>
+  );
+};
+
+export default App;
