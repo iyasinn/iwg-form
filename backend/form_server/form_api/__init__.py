6bd7d0f45a59084501500c25486e9049c878f12c
iyasin <iayasin771@gmail.com>
2024-01-02 23:41:16 -0500

    Production allow requests from frontend domain

--- backend/form_server/form_api/__init__.py
+++ backend/form_server/form_api/__init__.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 import json
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 print(__name__)
 
@@ -49,8 +49,6 @@ def upload_form():
     # print(response)
 
     return response, response_code
-
-
 
 
 @app.route("/get_fields", methods=["POST"])
