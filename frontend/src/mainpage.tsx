1c7a0ec7ad21718e4d605172e6194ebaa32a8065
khanabdu25 <khanabdu@umich.edu>
2024-01-06 00:19:15 -0500

    Animated MIUI dialog box
     - Using Dialog component with Slide transition

--- frontend/src/mainpage.tsx
+++ frontend/src/mainpage.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./mainpage.css";
 // import "./formcomponents.css";
 import LOGO_PATH from "./assets/tmc-logo.jpeg";
 import LEARNMORE_PATH from "./assets/help-outline.svg";
 import Form from "./components/Form/Form";
+import Dialog from "@mui/material/Dialog";
+import { TransitionProps } from "@mui/material/transitions";
+import { Slide } from "@mui/material";
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const MainPage: React.FC = () => {
   // const formRef = useRef<HTMLFormElement>(null);
   // const [complete, onComplete] = useState(false);
   //const [value, onChange] = useState<Value>(new Date());
+  const [infoRequested, setinfoRequested] = useState(false);
+
+  const handleInfoRequestedToggle = () => {
+    setinfoRequested((prevState) => !prevState);
+  };
 
   return (
     <main id="mainpage">
@@ -16,9 +33,7 @@ const MainPage: React.FC = () => {
         <div style={{ display: "flex", alignItems: "center" }}>
           <h1 className="form-heading">Report an Incident</h1>
           <button
-            onClick={() => {
-              console.log("user requested help");
-            }}
+            onClick={handleInfoRequestedToggle}
             style={{
               border: "none",
               background: "none",
@@ -51,7 +66,25 @@ const MainPage: React.FC = () => {
         />
       </div>
 
-      <Form />
+      {infoRequested ? (
+        <Dialog
+          open={infoRequested}
+          TransitionComponent={Transition}
+          onClose={handleInfoRequestedToggle}
+          maxWidth="lg" // Set maximum width
+          fullWidth={true} // Dialog takes the full width of the screen
+          PaperProps={{
+            style: {
+              minHeight: "80vh", // Minimum height of the dialog
+              maxHeight: "90vh", // Maximum height of the dialog
+              width: "80%", // Set custom width
+              // Add more custom styles if needed
+            },
+          }}
+        ></Dialog>
+      ) : (
+        <Form />
+      )}
     </main>
   );
 };
